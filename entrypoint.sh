40f9e728241af789df2fdca5d180c539cd206ac0
dank1775 <fabianepico@gmail.com>
2026-03-01 20:13:39 -0600

    removed arix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,21 +9,6 @@ sleep 10
 echo "Ejecutando migraciones de base de datos..."
 php artisan migrate --force --seed --step
 
-# artix installation check
-echo "Verificando instalación de Arix..."
-#run artix command, if it fails, run migrations and optimizations
-php artisan arix || ( \
-    echo "Fallo en 'php artisan arix' o ya instalado, ejecutando reparación/migración..." && \
-    php artisan migrate --force && \
-    php artisan optimize:clear && \
-    php artisan optimize \
-)
-
-# artix perms
-echo "Corrigiendo permisos..."
-chmod -R 755 /app/storage/* /app/bootstrap/cache
-chown -R nginx:nginx /app/storage /app/bootstrap/cache
-
 
 echo "Iniciando Pterodactyl..."
 exec "$@"
